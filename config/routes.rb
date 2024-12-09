364b7bc43c7782d8c8b40eb828a91cac9edcbb94
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-12-09 00:53:59 -0300

    Remove comments and add namespace to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
-
   get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api, defaults: { format: :json } do
+  end
 end
