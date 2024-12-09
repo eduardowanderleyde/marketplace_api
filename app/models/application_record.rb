f16e6d84da108899d578d85738d51e1af993b985
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-12-09 00:16:02 -0300

    Setup test and documentation for test

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# ApplicationRecord serves as the base class for all models in the Rails application.
+# It is an abstract class, meaning it is not intended to be instantiated directly.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
