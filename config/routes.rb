f1ca82a4f6d1373378c44410ed662461105053d3
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-12-13 12:50:22 -0300

    Modified routes, cors, and add gems to project

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
   get 'up' => 'rails/health#show', as: :rails_health_check
+
   namespace :api, defaults: { format: :json } do
+    scope constraints: { subdomain: 'api' }, path: '/' do
+      scope module: :v1,
+                constraints: ApiConstraints.new(version: 1, default: true) do
+      end
+    end
   end
 end
